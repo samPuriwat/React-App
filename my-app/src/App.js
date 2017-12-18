316df6d06ade141b18e810dd3909abdfd7602a15
samPuriwat <sam.puriwat@gmail.com>
2017-12-18 15:04:02 +0700

    Create Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,17 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render(){
+    var mystyle={
+      fontSize:100,
+      color:'#FF0000'
+    }
     return(
       <div>
-        <h1>React Tutorial samPuriwat</h1>
-
+        <h1 style = {mystyle}>React Tutorial samPuriwat</h1> 
+        <h2>{50*50}</h2>
       </div>
-
     );
   }
-
-
 
 }//App
 
