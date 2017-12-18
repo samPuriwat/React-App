be43954960a4312525cd7392f52ed21c7483838f
samPuriwat <sam.puriwat@gmail.com>
2017-12-18 16:16:31 +0700

    State

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 
 class Header extends Component{
+    constructor(){
+        super();
+        this.state = {
+            name: "UnLike",
+            count: 0
+        };
+    }
     render(){
+        setTimeout(()=>{
+        this.setState({name:"Like"});
+    },2000);
+    setInterval(()=>{
+        this.setState({count: this.state.count+1});
+    },100);
+
+
       return(
         <div>
             <h1>samPuriwat React Tutorial</h1>
+            <h2>{this.state.name}</h2>
+            <h2>{this.state.count}</h2>
         </div>
        
       );
     }
   
   }
-  export default Header;
+ export default Header;
