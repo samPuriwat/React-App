13fe73dd10e66f43a32694bf90a99a32fc717dd6
samPuriwat <sam.puriwat@gmail.com>
2017-12-18 14:43:31 +0700

    Test React Github

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <h1>React Tutorial samPuriwat</h1>
+
+      </div>
+
+    );
+  }
+
+
+
+}//App
+
+export default App;
